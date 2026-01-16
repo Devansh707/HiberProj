dfcf56aedf564b8442a6210c315070d1646e724a
Devansh707 <devanshsrivastava707@gmail.com>
2026-01-17 01:26:22 +0530

    Fetching Data from DB using Hibernate.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.internal.build.AllowSysOut;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -14,19 +15,27 @@ public class Main {
         st.setsAge(22);
         st.setsName("Harry12 ");
 
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(org.example.Student.class);
-        cfg.configure();
-        SessionFactory sf = cfg.buildSessionFactory();
+        Student s2 = null;
+
+//        Configuration cfg = new Configuration();
+//        cfg.addAnnotatedClass(org.example.Student.class);
+//        cfg.configure();
+//        SessionFactory sf = cfg.buildSessionFactory();
+        SessionFactory sf = new Configuration().addAnnotatedClass(Student.class).configure().buildSessionFactory();
 
         Session session = sf.openSession();
 
-        Transaction transaction = session.beginTransaction();
+//        Transaction transaction = session.beginTransaction();
+        s2 = session.get(Student.class, 12);
 
         session.save(st);
 
-        transaction.commit();
+//        transaction.commit();
         System.out.println(st);
 
+        System.out.println(s2);
+
+        session.close();
+        sf.close();
     }
 }
